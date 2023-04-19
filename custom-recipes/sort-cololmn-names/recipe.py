e4ec30f6b00573a7aa11819c4132943f7a66c433
admin <neba.nfonsang@dataiku.com>
2023-04-18 23:05:31 -0600

    Edited files

--- custom-recipes/sort-cololmn-names/recipe.py
+++ custom-recipes/sort-cololmn-names/recipe.py
@@ -14,19 +14,4 @@ input_dataset = dataiku.Dataset(input_dataset_name)
 # Get input output datasets
 output_dataset_name = get_output_names_for_role('output_dataset')[0]
 output_dataset = dataiku.Dataset(output_dataset_name)
-# Get parameters
-start_index_at_1 = get_recipe_config()['start_index_at_1']
-record_id = get_recipe_config().get('record_id_name', "RecordID") #Gets parameter value which is the name of a column and if not provided, the default name “RecordID” is used. Note mandatory was set to false in recipe.json file.
 
-
-# Read recipe inputs
-
-df = input_dataset.get_dataframe()
-
-if start_index_at_1:
-    df.insert(0,record_id, range(1, len(df.index) + 1))
-else:
-    df.insert(0,record_id, range(len(df.index)))
-
-# Write recipe outputs
-output_dataset.write_with_schema(df)
