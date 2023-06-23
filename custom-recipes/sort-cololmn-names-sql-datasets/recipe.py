3659db58c7c3000c3b6b903c2ef22b701523a7bc
admin <neba.nfonsang@dataiku.com>
2023-06-23 15:19:45 -0600

    Edited file 'custom-recipes/sort-cololmn-names-sql-datasets/recipe.py'

--- custom-recipes/sort-cololmn-names-sql-datasets/recipe.py
+++ custom-recipes/sort-cololmn-names-sql-datasets/recipe.py
@@ -8,11 +8,11 @@ from dataiku.customrecipe import get_input_names_for_role
 from dataiku.customrecipe import get_output_names_for_role
 from dataiku.customrecipe import get_recipe_config
 
-# Get input datasets
+# Get input SQL dataset
 input_dataset_name = get_input_names_for_role('input_dataset')[0]
 input_dataset = dataiku.Dataset(input_dataset_name)
 
-# Get input output datasets
+# Get input output dataset
 output_dataset_name = get_output_names_for_role('output_dataset')[0]
 output_dataset = dataiku.Dataset(output_dataset_name)
 
